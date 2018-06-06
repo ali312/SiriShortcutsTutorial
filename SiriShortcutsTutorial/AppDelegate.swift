4aa0581e685342fab183cbd1d32ad530430df5cf
ali312 <ali312@yandex.ru>
2018-06-06 19:12:53 +0600

    initial commit

--- SiriShortcutsTutorial/AppDelegate.swift
+++ SiriShortcutsTutorial/AppDelegate.swift
@@ -19,6 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        let viewController = window?.rootViewController as! ViewController
+        viewController.makeViewRed()
+        
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- SiriShortcutsTutorial/ViewController.swift
+++ SiriShortcutsTutorial/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
-
+import Intents
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func buttonAction(sender: UIButton) {
+        let activity = NSUserActivity(activityType: "com.thelightprj.SiriShortcutsTutorial.makeRed")
+        activity.title = "Make View Red"
+        activity.userInfo = ["color" : "red"]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(rawValue: "com.thelightprj.SiriShortcutsTutorial.makeRed")
+        self.view.userActivity = activity
+        activity.becomeCurrent()
+        
+        makeViewRed()
+    }
+    
+    public func makeViewRed() {
+        self.view.backgroundColor = .red
+    }
 }
 
