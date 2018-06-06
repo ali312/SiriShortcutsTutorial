f364d17ac618f8e57324a3af8c838ceaa08695bd
ali312 <ali312@yandex.ru>
2018-06-06 18:02:11 +0600

    Initial Commit

---
+++ SiriShortcutsTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutsTutorial
+//
+//  Created by Anton Kuznetsov on 06/06/2018.
+//  Copyright © 2018 Anton Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
